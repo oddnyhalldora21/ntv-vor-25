5f6d909de4bcd39ae6f8cb5753705456106af775
oddnyhalldora21 <oddnyhalldora21@gmail.com>
2025-02-24 16:16:35 +0100

    Create assignement_1.dart

---
+++ cheatSheet/assignement_1.dart
@@ -0,0 +1,78 @@
+
+/* //1
+
+void main (List<String> arguments) {
+  String firstName = "Oddny Halldora";
+  String lastName = "Oddsdottir";
+  print("Hello");
+  String fullName = firstName + " " + lastName;
+  print( "Hello $fullName") ;
+
+}
+
+ */
+
+/* //2.
+
+import 'dart:io';
+
+void main() {
+
+  print("Enter a number:");
+  int number1 = int.parse(stdin.readLineSync()!);
+
+  print("Enter a second number:");
+  int number2 = int.parse(stdin.readLineSync()!);
+
+  int sumResult = number1 + number2;
+
+  print("$number1 + $number2 equals $sumResult");
+}
+
+
+ */
+
+/*
+// 3.
+
+import 'dart:io';
+
+void main() {
+
+  print("Skrifaðu tölustaf:");
+  int a = int.parse(stdin.readLineSync()!);
+
+  print("Skrifaðu annann tölustaf:");
+  int b = int.parse(stdin.readLineSync()!);
+
+  int result = a ~/ b;
+
+  print("$a deilt með $b er $result");
+}
+
+*/
+
+/*
+//4.
+void main() {
+  print("-1 + 5 * 5 = ");
+  print(-1 + 5 * 5);
+
+  print(" ");
+  
+  print("(25 + 5) % 4 =");
+  print((25 + 5) % 4);
+
+  print(" ");
+
+  print("8 + 6 * 4 / 2 =");
+  print(8 + 6 * 4 / 2);
+
+  print(" ");
+
+  print("2 + 15 / 6 * 1 - 7 % 2 =") ;
+  print(2 + 15 / 6 * 1 - 7 % 2);
+}
+
+
+ */
